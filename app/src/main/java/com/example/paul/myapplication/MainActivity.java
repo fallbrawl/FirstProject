39c3fa8d135ab31ae5b2bbf5938c506717b8f4ee
Fallbrawl <herdemismyrise.bt@gmail.com>
2016-10-03 17:45:39 +0300

    smth

--- app/src/main/java/com/example/paul/myapplication/MainActivity.java
+++ app/src/main/java/com/example/paul/myapplication/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 
 import com.example.paul.myapplication.R;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         display(1);
+        displayPrice(2 * 5);
     }
 
     /**
@@ -36,5 +39,10 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
